5eecce5b0e9f8d00781bccd40a6b085c4189fc8d
cooderyg <ygk9604@naver.com>
2023-06-10 15:53:08 +0900

    14-01

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { AppProps } from "next/app";
+import Layout from "../src/commons/layout";
 
 export default function App({ Component }: AppProps): JSX.Element {
   const client = new ApolloClient({
@@ -11,7 +12,9 @@ export default function App({ Component }: AppProps): JSX.Element {
     <>
       <div>======여기는 _app.js 컴포넌트 시작부분 입니다.=======</div>
       <ApolloProvider client={client}>
-        <Component />
+        <Layout>
+          <Component />
+        </Layout>
       </ApolloProvider>
       <div>========여기는 _app.js 컴포넌트 마지막부분 입니다.========</div>
     </>
