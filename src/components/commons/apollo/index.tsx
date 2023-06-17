192344b1d11a3cea771fc11905eabb8cbf502337
cooderyg <ygk9604@naver.com>
2023-06-17 17:19:20 +0900

    22-02

--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -6,6 +6,9 @@ import {
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 
+// 재렌더링 때문에 cache가 날라가기 때문에 컴포넌트 위에 만든 상수
+const GLOBAL_STATE = new InMemoryCache();
+
 interface IApolloSettingProps {
   children: JSX.Element;
 }
@@ -18,7 +21,7 @@ export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
   // const authLink
   const client = new ApolloClient({
     link: ApolloLink.from([uploadLink]),
-    cache: new InMemoryCache(), // 컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아보기!
+    cache: GLOBAL_STATE, // 컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아보기!
   });
   // prettier-ignore
   return (
---
+++ src/components/units/22-fetch-policy/index.tsx
@@ -0,0 +1,36 @@
+import { gql, useQuery } from "@apollo/client";
+import {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../commons/types/generated/types";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards {
+    fetchBoards {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function FetchPolicyExample(): JSX.Element {
+  const { data } = useQuery<Pick<IQuery, "fetchBoards">, IQueryFetchBoardsArgs>(
+    FETCH_BOARDS,
+    {
+      fetchPolicy: "cache-first", // fetchPolicy: "network-only" 무조건 다시 api요청
+    }
+  );
+
+  return (
+    <>
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span style={{ margin: "10px" }}>{el.title}</span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+    </>
+  );
+}
